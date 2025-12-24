966665ba2e07423a0723a86f00c2399ad64f14c4
GOAT <lucasonyiego283@gmail.com>
2025-12-24 15:15:19 +0300

    re-writing everything

--- main.c
+++ main.c
@@ -3,89 +3,12 @@
 #include "nmap.h"
 
 
-#define ARP_SENDER_THREADS 10
-#define INTERFACE "wlan0"
-
 
 int main() {
   
 
 
-    in_addr_t ip, mask;
-    unsigned char mac[6];
     
-    if (get_iface_ip_mask(INTERFACE, &ip, &mask, mac) == -1) {
-        fprintf(stderr, "get_iface_ip_mask failed: %s\n", strerror(errno));
-        return 1;
-    }
-    compute_subnet_range(ip, mask);
-    char ip_str[INET_ADDRSTRLEN], mask_str[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &ip, ip_str, sizeof(ip_str));
-    inet_ntop(AF_INET, &mask, mask_str, sizeof(mask_str));
-
-    int8 *start_ip=network_to_presentation(start_ip_address);
-    int8 *end_ip=network_to_presentation(end_ip_address);
-
-    printf("Interface IP: %s\n", ip_str);
-    printf("Netmask:      %s\n", mask_str);
-    printf("Host range:   %s - %s\n",start_ip,end_ip);
-
-   free(start_ip);
-   free(end_ip);
-
-    int arp_sock = socket(AF_PACKET, SOCK_RAW, htons(ARP_PROTOCAL));
-    if (arp_sock < 0) {
-        perror("socket");
-        return 1;
-    }
-       
-    pthread_t arp_listener;
-    pthread_create(&arp_listener, NULL, listen_arp_replies, (void *)(uintptr_t)arp_sock);
-     
-    
-
-    in_addr_t total_hosts = ntohl(end_ip_address) - ntohl(start_ip_address) + 1;
-    in_addr_t hosts_per_thread = total_hosts / ARP_SENDER_THREADS;
-    
-    pthread_t sender_threads[ARP_SENDER_THREADS];
-    
-    for (int i = 0; i < ARP_SENDER_THREADS; i++) {
-        arp_sender_args_t *args = malloc(sizeof(arp_sender_args_t));
-        args->sock = arp_sock;
-        args->src_ip = ip;
-        memcpy(args->mac, mac, 6);
-        args->iface = INTERFACE;
-    
-        in_addr_t start = ntohl(start_ip_address) + i * hosts_per_thread;
-        in_addr_t end = (i == ARP_SENDER_THREADS - 1)
-            ? ntohl(end_ip_address)
-            : start + hosts_per_thread - 1;
-    
-        args->start_ip = htonl(start);
-        args->end_ip = htonl(end);
-    
-        pthread_create(&sender_threads[i], NULL, arp_sender_thread, args);
-    }
-
-
-    
-   for (int i = 0; i < ARP_SENDER_THREADS; i++) {
-      pthread_join(sender_threads[i], NULL);
-  }
-
-
-          
-    for (int i = 0; i < MAXTHREADPOOL; i++) {
-            pthread_join(pool[i], NULL);
-        }
-
-
-   
-
-   
-    pthread_cancel(arp_listener);
-    pthread_join(arp_listener, NULL);
-    close(arp_sock);
      
     return 0;
 }
@@ -109,3 +32,9 @@ int main() {
 //banidit13:FO5dwFsc0cbaIiH0h8J2eUks2vdTDwAn
 //bandit15:8xCjnmgoKbGLhHFAZlGE5Tmu4M2tKJQo
 //bandit16:kSkvUpMQ7lBYyCM4GBPvCvT1BfWRy0Dx
+//bandit18:x2gLTTjFwMOhQ8oWNbMN362QKxfRqGlO
+//bandit19:cGWpMaKXVwDUNgPAVJbWYuGHVn9zl3j8
+//bandit20:0qXahG8ZjOVMN9Ghs7iOWsCfZyXOUbYO
+//bandit21:EeoULMCra2q0dSkYj561DX7s1CpBuOBt
+//bandit22:tRae0UfB9v0UzbCdn9cY0gQnds9GF58Q
+//bandit23:tRae0UfB9v0UzbCdn9cY0gQnds9GF58Q
--- nmap.h
+++ nmap.h
@@ -1,110 +1,16 @@
 #ifndef NMAP_H
 #define NMAP_H
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stddef.h>
-#include <stdbool.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include<stdlib.h>
-#include <net/if.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <errno.h>
-#include <arpa/inet.h>
-#include <sys/time.h>
-#include <netinet/ip.h>
-#include <sys/socket.h>
-#include <netpacket/packet.h>
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-
-#define PROTOSIZE 4
-#define  MACSIZE  6
-
-#define MAXTHREADPOOL 30
-#define PORTSPERTASK 100
-#define ARPHTYPE_ETHER 1
-#define ETHERTYPE_IP 0x0800
-#define  ARPOPCODE_REQUEST 1
-#define ARPOPCODE_REPLY 2
-#define ARP_PROTOCAL 0x0806
-#define MAXPORT 65535
 
 
 
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-typedef uint8_t uint8;
-typedef char int8;
-
-extern pthread_t pool[MAXTHREADPOOL];
-
-typedef struct {
-   in_addr_t ip;
-   uint16_t start_port;
-   uint16_t end_port;
-} port_range_t;
+typedef char i8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef unsigned long int u64;
 
 
-typedef struct{
-     in_addr_t ip;
-     uint16 start_port;
-     uint16 end_port;
-}port_scan_args_t;
 
-typedef struct ether_arp{
-     uint16 HTYPE;
-     uint16 PTYPE;
-     uint8 HLEN;
-     uint8 PLEN;
-     uint16 OPCODE;
-      
-     uint8 SHA[MACSIZE];
-     uint8 SPA[PROTOSIZE];
-     uint8 THA[MACSIZE];
-     uint8 TPA[PROTOSIZE];
-     
-
-}__attribute__((packed)) ether_arp;
-
-typedef struct ether_header{
-    uint8 dst_mac[MACSIZE];
-    uint8 src_mac[MACSIZE];
-    uint16 ETHER_TYPE;
-
-}__attribute__((packed)) ether_header;
-
-
-typedef struct {
-   in_addr_t start_ip;
-   in_addr_t end_ip;
-   int sock;
-   unsigned char mac[6];
-   in_addr_t src_ip;
-   const char *iface;
-} arp_sender_args_t;
-
-
-// in_addr_t generate(void);
-extern in_addr_t start_ip_address, end_ip_address;
-extern pthread_t thread;
-int8 *network_to_presentation(in_addr_t);
-int get_iface_ip_mask(const char *, in_addr_t *, in_addr_t *mask, unsigned char *);
-void send_arp_packet(int sock, unsigned char *, in_addr_t , in_addr_t , const char *); 
-void *listen_arp_replies(void *);
-void compute_subnet_range(in_addr_t , in_addr_t ); 
-void *arp_sender_thread(void *) ;
-void* tcp_port_range_scan(void* arg) ;
-void* tcp_task_worker(void* arg);
 
 #endif
 
